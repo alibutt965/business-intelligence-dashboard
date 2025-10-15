89dfedc985970921612426376f584c645c0c60bb
alibutt965 <ibutt7391@gmail.com>
2025-10-15 15:36:22 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// --- MongoDB Connection ---
 const uri = "mongodb://root:toor@localhost:27017/?authSource=admin";
 
 
 mongoose
   .connect(uri)
-  .then(() => console.log("✅ Connected to MongoDB (Docker)"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+  .then(() => console.log("Connected to MongoDB (Docker)"))
+  .catch((err) => console.error(" MongoDB connection error:", err));
 
-// --- Sample schema & route ---
+
 const userSchema = new mongoose.Schema({ name: String, age: Number });
 const User = mongoose.model("User", userSchema);
 
@@ -31,3 +30,4 @@ app.post("/api/users", async (req, res) => {
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
+
